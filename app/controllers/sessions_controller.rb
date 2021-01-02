cc48a4ed9a8ca5b397bbb387dca1cc1ec035896f
Roberto Basulto <roberto.basulto085@gmail.com>
2021-01-02 15:33:43 -0500

    Can log in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,37 @@
+class SessionsController <  ApplicationController
+  def create
+    client_id = '2901152965a92c3b41d2'
+    client_secret = '259d58cb469d6b1519fa857c8375833da1d637f7'
+    code = params[:code]
+
+    conn = Faraday.new(url: 'https://github.com', headers: {'Accept': 'application/json'})
+
+    response = conn.post('/login/oauth/access_token') do |req|
+      req.params['code'] = code
+      req.params['client_id'] = client_id
+      req.params['client_secret'] = client_secret
+    end
+
+    data = JSON.parse(response.body, symbolize_names: true)
+    access_token = data[:access_token]
+
+    conn = Faraday.new(
+    url: 'https://api.github.com',
+    headers: {
+        'Authorization': "token #{access_token}"
+    }
+  )
+    response = conn.get('/user')
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    user          = User.find_or_create_by(uid: data[:id])
+    user.username = data[:login]
+    user.uid      = data[:id]
+    user.token    = access_token
+    user.save
+
+    session[:user_id] = user.id
+
+    redirect_to dashboard_path
+  end
+end
